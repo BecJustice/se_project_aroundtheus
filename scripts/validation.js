db238bb8a8fe82ae6ea113ac3eb0b2ecea71141a
rebecca justice <rebecca.bindraban@gmail.com>
2024-02-16 18:08:51 -0700

    js updates

--- scripts/validation.js
+++ scripts/validation.js
@@ -25,7 +25,7 @@ function checkInputValidity(formEl, inputEl, options) {
   }
 }
 
-function disableSubmitButton(submitButton, { inactiveButtonClass }) {
+/*function disableSubmitButton(submitButton, { inactiveButtonClass }) {
   submitButton.classList.add(inactiveButtonClass);
   submitButton.disabled = true;
 }
@@ -33,25 +33,33 @@ function disableSubmitButton(submitButton, { inactiveButtonClass }) {
 function enableSubmitButton(submitButton, { inactiveButtonClass }) {
   submitButton.classList.remove(inactiveButtonClass);
   submitButton.disabled = false;
-}
+} */
 
 function hasInvalidInput(inputList) {
   return !inputList.every((inputEl) => inputEl.validity.valid);
 }
 
 function toggleButtonState(inputEls, submitButton, { inactiveButtonClass }) {
-  if (hasInvalidInput(inputEls)) {
-    return disableSubmitButton(submitButton, { inactiveButtonClass });
+  let foundInvalid = false;
+  inputEls.forEach((inputEl) => {
+    if (!inputEl.validity.valid) {
+      foundInvalid = true;
+    }
+  });
+
+  if (foundInvalid) {
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.disabled = true;
+  } else {
+    submitButton.classList.remove(inactiveButtonClass);
+    submitButton.disabled = false;
   }
-  enableSubmitButton(submitButton, { inactiveButtonClass });
 }
 
 function setEventListeners(formEl, options) {
   const { inputSelector } = options;
   const inputEls = [...formEl.querySelectorAll(inputSelector)];
   const submitButton = formEl.querySelector(options.submitButtonSelector);
-  toggleButtonState(inputEls, submitButton, options);
-
   inputEls.forEach((inputEl) => {
     inputEl.addEventListener("input", (e) => {
       checkInputValidity(formEl, inputEl, options);
